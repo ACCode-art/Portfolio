08d6dbbc3c8c3530a78d7c24fe2c44807bf0f396
Adam cooke <adam-cooke@live.co.uk>
2021-05-20 18:59:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const projectInformation = [
+  {
+    title: 'Youtube (Desktop-Clone)',
+    desc:
+      "ou don't know me son. Suffering is a test. That’s all it is. Suffering is the true test of life. The most important conversations you’ll ever have are the ones you’ll have with yourself. You wake up with them",
+    img: '',
+  },
+];
